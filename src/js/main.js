b42abcf944fd3dc08bbc1b3f5a7485dcd9c2a7de
Yeldos <yeldos23@yandex.ru>
2020-03-18 14:05:36 +0600

    add modal js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,32 @@
 document.addEventListener("DOMContentLoaded", function (event) {
   const modal = document.querySelector('.modal');
-  console.log(modal);
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
+
+  const switchModal = () => {
+    modal.classList.toggle('modal--visible');
+  };
+
+  const closeModal = () => {
+    modal.classList.remove('modal--visible')
+  }
+
+  modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal);
+  });
+
+  closeBtn.addEventListener('click', switchModal);
+
+  document.addEventListener('click', function(e) {
+    if (e.target == modal) {
+      closeModal();
+    }
+  });
+  document.addEventListener('keydown', function (event) {
+    closeModal();
+  });
+
 });
+
+
+
