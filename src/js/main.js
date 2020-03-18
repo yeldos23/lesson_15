8cee892cfaf245bb6bdc4cd1e7faaefc366b2fc0
Yeldos <yeldos23@yandex.ru>
2020-03-18 15:42:26 +0600

    add jquery

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,11 @@
+/*
 document.addEventListener("DOMContentLoaded", function (event) {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
-
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
   };
-
   const closeModal = () => {
     modal.classList.remove('modal--visible')
   }
@@ -22,9 +21,41 @@ document.addEventListener("DOMContentLoaded", function (event) {
       closeModal();
     }
   });
+
   document.addEventListener('keydown', function (event) {
     closeModal();
   });
+
+});
+*/
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+    }
+  );
+
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+    }
+  );
+  $(document).on('keydown', function (event) {
+    if (event.code == 'Escape') {
+      modal.removeClass('modal--visible');
+      }
+    }
+  );
+  $(document).on('click', '.modal', function () {
+      modal.removeClass('modal--visible'); 
+      }
+  );
+
+
 
 });
 
