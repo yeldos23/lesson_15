c2374a22ccee401c769b57bc2096c0d47403513c
Yeldos <yeldos23@yandex.ru>
2020-03-20 00:07:07 +0600

    add swiper

--- src/js/main.js
+++ src/js/main.js
@@ -69,6 +69,32 @@ $(document).ready(function () {
     }, delay);
   });
 
+  //initialize swiper when document ready
+  var mySwiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    }, 
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+
+  });
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() + 10 + bullets.width() + 10 );
+  bullets.css('left', prev.width() + 10 );
+  
+
 });
 
 
