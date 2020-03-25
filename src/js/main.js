12525c6874beed8fa569d38116b86bfa14b72062
Yeldos <yeldos23@yandex.ru>
2020-03-25 16:51:54 +0600

    fix control button and div.invalid

--- src/js/main.js
+++ src/js/main.js
@@ -115,7 +115,7 @@ $(document).ready(function () {
 
       userPhone: {
         required: true,
-        minlength: 10
+        minlength: 17
       },
       
       userEmail: {
@@ -154,7 +154,7 @@ $(document).ready(function () {
 
       userPhone: {
         required: true,
-        minlength: 10
+        minlength: 17
       },
 
       userEmail: {
@@ -193,7 +193,7 @@ $(document).ready(function () {
 
       userPhone: {
         required: true,
-        minlength: 10
+        minlength: 17
       },
 
       userEmail: {
@@ -223,6 +223,39 @@ $(document).ready(function () {
   // Маска для телефона
   $('[type=tel]').mask('+7(000) 000-00-00', { placeholder: "+7 (___) ___-__-__"});
 
+  // Создаем Yandex Карты
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+      center: [47.244729, 39.723187],
+      zoom: 17
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+        hintContent: 'Наш офис',
+        balloonContent: 'Вход со двора'
+      }, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#image',
+        // Своё изображение иконки метки.
+        iconImageHref: 'img/location.png',
+        // Размеры метки.
+        iconImageSize: [32, 32],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-5, -38]
+      });
+
+    myMap.geoObjects
+      .add(myPlacemark)
+  });
 
 });
 
