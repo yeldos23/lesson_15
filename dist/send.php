19e13db99ba2d70f035ebd500a04ddab91e0fbe2
Yeldos <yeldos23@yandex.ru>
2020-03-28 03:27:22 +0600

    optimization

---
+++ dist/send.php
@@ -0,0 +1,43 @@
+<?php
+
+$userName = $_POST['userName'];
+$userEmail = $_POST['userEmail'];
+$userPhone = $_POST['userPhone'];
+
+// Load Composer's autoloader
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php'; 
+require 'phpmailer/Exception.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'repairdesignsite@gmail.com';                     // SMTP username
+    $mail->Password   = 'riddik10';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 465;                                    // TCP port to connect to
+    $mail->CharSet = "utf-8";
+
+    //Recipients
+    $mail->From = ('repairdesignsite@gmail.com');
+    $mail->addAddress('info@sabyrbayev.kz');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Новая заявка с сайта';
+    $mail->Body    = "Имя пользователя: ${userName}. Телефон: ${userPhone}. Его почта: ${userEmail}";
+
+    if ($mail->send()) {
+        echo "ok";
+    } else {
+        echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+    }
+} catch (Exception $e) {
+    echo "Письмо не отправлено, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
-const {src, dest, watch} = require('gulp');
+const {src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const minify = require('gulp-minify');
+const htmlmin = require('gulp-htmlmin');
+const tinypng = require('gulp-tinypng-compress');
 
 // Static server
 function bs() {
@@ -30,4 +34,54 @@ function serveSass() {
     .pipe(browserSync.stream());
 }
 
+  function buildCSS(done) {
+    src('src/css/**/**.css')
+      .pipe(cleanCSS({compatibility: 'ie8'}))
+      .pipe(dest('dist/css/'));
+    done();
+  }
+
+  function buildJS(done) {
+    src(['src/js/**.js', '!src/js/**.min.js'])
+        .pipe(minify({ext: {
+                min: '.js'
+              }
+            }))
+        .pipe(dest('dist/js/'));
+    src('src/js/**.min.js').pipe(dest('dist/js/'));
+    done();
+  }
+
+  function html(done) {
+    src('src/**.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(dest('dist/'));
+    done();
+  }
+
+  function php(done) {
+    src('src/**.php')
+      .pipe(dest('dist/'));
+    src('src/phpmailer/**/**')
+      .pipe(dest('dist/phpmailer/'));
+    done();
+  }
+
+  function fonts(done) {
+    src('src/fonts/**/**')
+      .pipe(dest('dist/fonts/'));
+    done();
+  }
+
+  function imagemin(done) {
+    src('src/img/**/**')
+      .pipe(tinypng({ key: 'GCQzTNlbrJ72HP9shNrzSVjfFFbbxls7' }))
+      .pipe(dest('dist/img/'));
+    src('src/img/**/*.svg')
+      .pipe(dest('dist/img/'));
+    done();
+    
+  }
+
 exports.serve = bs;
+exports.build = series(buildCSS, buildJS, html, php, fonts, imagemin);
